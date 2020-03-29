155a27349ec3d094eebaca6c0c7f2bb4657b2548
xin-hui <xin-hui@users.noreply.github.com>
2020-03-29 19:19:45 +0800

    update workflow

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,15 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    //Key in your database details
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: ""
-  };
+  apiKey: "AIzaSyD7qw5NOQQXHU1fsvDx5YCwocSsqZOaeS0",
+  authDomain: "groceryproject-3b802.firebaseapp.com",
+  databaseURL: "https://groceryproject-3b802.firebaseio.com",
+  projectId: "groceryproject-3b802",
+  storageBucket: "groceryproject-3b802.appspot.com",
+  messagingSenderId: "130156783632",
+  appId: "1:130156783632:web:0bacb35fae7ede05e44295",
+  measurementId: "G-LZ65HVX05P"
+};
   
   firebase.initializeApp(firebaseConfig);
   var database = firebase.firestore();
